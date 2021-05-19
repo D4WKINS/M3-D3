bb2d1525219d8e8c48210165eeb378e7527ba8a2
Maksym Sharin <Maksym.sharin.work@gmail.com>
2021-05-19 16:34:11 +0200

    feat: ex1

--- script.js
+++ script.js
@@ -4,21 +4,40 @@
 window.onload = ()=> {
 
 //functions
-const getImages = async () =>{
+const getImages = async ( query) =>{
 let data = {}
-await fetch("http://www.splashbase.co/api/v1/images/search?query=" +"laptops")
+await fetch("http://www.splashbase.co/api/v1/images/search?query=" + query )
     .then( res =>  res.json() )
     .then( parsed => data = parsed)
-return data.images
-}
-const loadImages1 = () =>{
 
+    let imgUrlsArr = []
+    data.images.forEach(  el => imgUrlsArr.push(el.url) )
+    console.log( imgUrlsArr )
+return imgUrlsArr
+}
+
+const loadImages1 = () =>{
+    let targets = [...$('.card svg')]
+    getImages("laptops").then(
+        ( imgUrlsArr ) =>{
+            imgUrlsArr.forEach(
+                ( el , i ,arr) =>{
+                   let imgNode = document.createElement('img');
+                   imgNode.setAttribute("src", el )
+                   try {
+                       targets[i].outerHTML = imgNode.outerHTML
+                   }catch (e) {}
+                }
+            )
+
+        }
+    )
 }
 
 //Dom elements
 let loadImagesBtn = $('main .btn')[0]
 
 // function assignments
-loadImagesBtn.addEventListener( 'click', getImages )
+loadImagesBtn.addEventListener( 'click', loadImages1 )
 
 }
