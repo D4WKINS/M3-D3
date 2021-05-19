b048b78bbf208dd4b71f80129a2b2c102280202a
Maksym Sharin <Maksym.sharin.work@gmail.com>
2021-05-19 15:52:16 +0200

    feat: getImage full

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 
 
-// window.onload =()=>{
-// getImages()
-// }
 
 window.onload = ()=> {
+
+//functions
 const getImages = async () =>{
-let data = await fetch("http://www.splashbase.co/api/v1/images/search?query=" +"laptops")
-data =  data.json()
-console.log(data)
+let data = {}
+await fetch("http://www.splashbase.co/api/v1/images/search?query=" +"laptops")
+    .then( res =>  res.json() )
+    .then( parsed => data = parsed)
+return data.images
 }
 const loadImages1 = () =>{
 
 }
-//load to UI
-$('main .btn')[0].addEventListener(
-'click', getImages
-)
+
+//Dom elements
+let loadImagesBtn = $('main .btn')[0]
+
+// function assignments
+loadImagesBtn.addEventListener( 'click', getImages )
 
 }
