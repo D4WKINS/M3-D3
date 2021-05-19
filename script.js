7877bae8796d64531dae7f6dcc5ce2a9b3c22200
Carl <78683271+D4WKINS@users.noreply.github.com>
2021-05-19 12:41:10 +0000

    Update M3-D3

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+// window.onload =()=>{
+// getImages()
+// }
+
+const getImages = async () =>{
+fetch("http://www.splashbase.co/api/v1/images/search?query=" +"laptops")
+const data = await data.json()
+const images2 = await console.log(images2)
+}
+
+
+
+const loadImages1 = () =>{
+
+}
+
