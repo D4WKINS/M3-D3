c6b8c109dcbdf3895e39cbbba9d9c81035f2b3e0
Maksym Sharin <Maksym.sharin.work@gmail.com>
2021-05-19 15:03:36 +0200

    fix: getImage Works now

--- script.js
+++ script.js
@@ -4,15 +4,18 @@
 // getImages()
 // }
 
+window.onload = ()=> {
 const getImages = async () =>{
-fetch("http://www.splashbase.co/api/v1/images/search?query=" +"laptops")
-const data = await data.json()
-const images2 = await console.log(images2)
+let data = await fetch("http://www.splashbase.co/api/v1/images/search?query=" +"laptops")
+data =  data.json()
+console.log(data)
 }
-
-
-
 const loadImages1 = () =>{
 
 }
+//load to UI
+$('main .btn')[0].addEventListener(
+'click', getImages
+)
 
+}
