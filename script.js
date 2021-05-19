f6addc913f0bb6b921573bb8924932417e00ce08
Carl <78683271+D4WKINS@users.noreply.github.com>
2021-05-19 14:52:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,28 @@
 window.onload = ()=> {
 
 //functions
+hideBtn()
 const getImages = async () =>{
 let data = {}
 await fetch("http://www.splashbase.co/api/v1/images/search?query=" +"laptops")
     .then( res =>  res.json() )
     .then( parsed => data = parsed)
 return data.images
-}
-const loadImages1 = () =>{
 
+}
+
+
+
+function hideBtn(){
+const editBtn = document.querySelectorAll('.card-body button:nth-child(2)')
+const cards = document.querySelectorAll(".col-md-4 .card")
+for(let i = 0; i<editBtn.length;i++){
+    editBtn[i].innerText="Hide"
+    editBtn[i].addEventListener("click",()=>{
+            cards[i].style.visibility ="hidden"
+    })
+}
+}
 }
 
 //Dom elements
@@ -20,5 +33,3 @@ let loadImagesBtn = $('main .btn')[0]
 
 // function assignments
 loadImagesBtn.addEventListener( 'click', getImages )
-
-}
